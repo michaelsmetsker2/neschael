3d16455090d60aae0ebe7fc692c0b4090bcd50df
Michael Metsker <michaelsmetsker2@gmail.com>
2025-12-22 07:19:57 -0800

    refactored vertical movement for higher precision, horizontal is broken slightly now

--- lib/player.s
+++ lib/player.s
@@ -3,55 +3,55 @@
 ; lib/player.s
 ;
 ; this handles the players movement
-;
+;1010000011101100
 
 ;reminder, may need to worry about walking off platforms
 
 .SCOPE Player
     ; --- Memory constants ---
-  targetVelocityX   = $20   ; Signed Fixed Point 4.4
-  velocityX         = $21   ; Signed Fixed Point 4.4
-  velocityY         = $22   ; Signed Fixed Point 4.4
-  
-    ; these will overflow at around 8 screens
-  positionX         = $23   ; Unsigned Fixed Point 12.4
-  positionY         = $25   ; Unsigned Fixed Point 12.4
 
-  spriteX           = $27   ; Unsigned Screen Coordinates
-  spriteY           = $28   ; Unsigned Screen Coordinates
+    ; velocity
+  targetVelocityX   = $20   ; Signed Fixed Point 8.8
+  velocityX         = $22   ; Signed Fixed Point 8.8
+  velocityY         = $24   ; Signed Fixed Point 8.8
+    ; Subpixel location
+  positionX         = $26   ; Unsigned Fixed Point 8.8
+  positionY         = $28   ; Unsigned Fixed Point 8.8
+    ; Screen location
+  spriteX           = $2A   ; Unsigned Screen Coordinates
+  spriteY           = $2B   ; Unsigned Screen Coordinates 
+    ; world position
+  scrollX           = $2C   ; Unisgned Fixed point 16.8
 
-    ; these could be combinded if there are less than 8 motion states
-  heading           = $29   ; See `.ENUM Heading`
-  motionState       = $2A   ; See `.ENUM MotionState`
+  motionState       = $2F   ; See `.ENUM MotionState`
 
-  animationFrame    = $2B
-  animationTimer    = $2C
+  animationFrame    = $30
+  animationTimer    = $31
+
+  playerFlags       = $32   ; holds various player flags in various bit positions
+                            ; 0-5 unused 
+                            ; 6   heading, 0 for right, 1 for left
+                            ; 7   if the player has been holding A since the start of the jump
 
     ; Initialization values
   .SCOPE Initial
-    VELOCITY_X = 0
-    VELOCITY_Y = 0
-    POSX_LO    = $00
-    POSX_HI    = $03
-    POSY_LO    = $F0
-    POSY_HI    = $08
-    SPRITE_X   = 143
-    SPRITE_Y   = 143
+    POSITION_X = $3000 ; unsigned 8.8
+    POSITION_Y = $8F00 ; unsigned 8.8
+    SPRITE_X   = 143   ; screen cords
+    SPRITE_Y   = 143   ; screen cords
   .ENDSCOPE
 
   .SCOPE Jump
-    FLOOR_HEIGHT = 143
-    INITIAL_VELOCITY = $C8
-    MAX_FALL_SPEED = $40
-    SLOW_FALL_SPEED  = $02   ; deceleration while holding A
-    BASE_FALL_SPEED  = $05   ; deceleration while free falling
-    DECELERATION_THRESHOLD = $E0 ; greater than this velocity, slow falling
-                                      ; will no longer be possible, 
+    FLOOR_HEIGHT           = 143   ; screen cords temp ============================
+    INITIAL_VELOCITY       = $FC00 ; signed fixed point 8.8 
+    SLOW_FALL_DECCEL       = $50   ; deceleration while holding A
+    BASE_FALL_DECCEL       = $70   ; deceleration while free falling
+    DECELERATION_THRESHOLD = $E0   ; greater than this velocity, slow falling
   .ENDSCOPE
 
   .SCOPE Velocities
-    RIGHT_WALK_TARGET = $28 ; signed 4.4 fixed point
-    LEFT_WALK_TARGET  = $D8 ; signed 4.4 fixed point
+    RIGHT_WALK_TARGET = $0028 ; signed 8.8 fixed point
+    LEFT_WALK_TARGET  = $80D8 ; signed 8.8 fixed point
   .ENDSCOPE
 
   .ENUM Heading ; direction player is facing
@@ -74,30 +74,33 @@
   .ENDPROC
 
   .PROC init_x
-      ; Start with no velocity
-    LDA #Initial::VELOCITY_X
+      ; zero velocity and target velocity
+    LDA #%00
     STA targetVelocityX
+    STA targetVelocityX+1
     STA velocityX 
+    STA velocityX+1
       ; Sets initial X-position to 110 or $06E0 in 12.4 fixed point
     LDA #Initial::SPRITE_X
     STA spriteX
-    LDA #Initial::POSX_LO
+    LDA #<Initial::POSITION_X
     STA positionX
-    LDA #Initial::POSX_HI
-    STA positionX + 1
-    RTS    
+    LDA #>Initial::POSITION_X
+    STA positionX+1
+    RTS   
   .ENDPROC
 
   .PROC init_y
-      ; start with no velocity
-    LDA #Initial::VELOCITY_Y
+      ; zero velocity
+    LDA #$00
     STA velocityY 
-      ; Sets initial Y-position to 110 or $06E0 in 12.4 fixed point
+    STA velocityY+1
+      ; Set initial Y-position
     LDA #Initial::SPRITE_Y
     STA spriteY
-    LDA #Initial::POSY_LO
+    LDA #<Initial::POSITION_Y
     STA positionY
-    LDA #Initial::POSY_HI
+    LDA #>Initial::POSITION_Y
     STA positionY + 1
     RTS
   .ENDPROC
@@ -131,87 +134,79 @@
       LDA motionState  
       CMP #MotionState::Airborne
       BEQ @airborne              ; branch if player is airborne
-    @check_jump:
+    @check_jump:                 ; can only start a new jump from the ground
       LDA btnPressed
       AND #_BUTTON_A
       BNE @begin_jump            ; branch if a new jump is detected
       LDA #0
-      STA velocityY    ; clear Y velocity in case landing? idk disabled temporarily see if it causes issues
+      STA velocityY              ; dont move if on the ground and not jumping ; this can be changed for colision implemenetations?
+      STA velocityY+1
       RTS
     @begin_jump:
-      LDA #Jump::INITIAL_VELOCITY
+      LDA playerFlags
+      ORA #%10000000               ; set the holding jump flag
+      STA playerFlags
+
+      LDA #<Jump::INITIAL_VELOCITY ; update vertical velocity
       STA velocityY
-      LDA #MotionState::Airborne
+      LDA #>Jump::INITIAL_VELOCITY
+      STA velocityY+1
+      LDA #MotionState::Airborne   ; set motionState to airborne
       STA motionState
       RTS
     @airborne:
       JSR update_jump_velocity
-      JSR apply_velocity_y
+      JSR apply_velocity_y        ; these are not in update so an early exit can save cpu cycles
       JSR bound_position_y
       RTS
     .endproc
 
-    .PROC update_jump_velocity
+    .PROC update_jump_velocity ; this updates mid air velocity
         ; Determine if velocity decelerates slow or fest based on button hold
-      LDY #Jump::BASE_FALL_SPEED
-      LDA velocityY
-      CMP #Jump::DECELERATION_THRESHOLD
-      BPL @decelerate ; fast fall if velocity over threshhold
+      LDY #0                      ; lookup table offset for BASE_FALL_SPEED
+      BIT playerFlags
+      BPL @decelerate             ; branch if held jump isn't set
       LDA btnDown
       AND #_BUTTON_A
-      BEQ @decelerate ; fast fall if a is not held down
-      LDY #Jump::SLOW_FALL_SPEED
-    @decelerate:
+      BEQ @newly_fast             ; branch if A isn't held
+        ; check velocity threshold
+      LDA velocityY+1
+      CMP #Jump::DECELERATION_THRESHOLD    ; NOTE: this only works as both are negative
+      BPL @newly_fast             ; branch if velocity is past threshhold
+      LDY #2                      ; SLOW_FALL_SPEED offset
+    @newly_fast:                  ; for first times using fast falling set flag
+      LDA playerFlags             ; updates the flag to save cpu cycles
+      AND #%01111111
+      STA playerFlags
+    @decelerate: 
         ; Perform the deceleration
-      TYA
-      CLC
+      CLC 
+      LDA fall_speeds,Y
       ADC velocityY
-        ; If I want to cap max fall speed, do it here -----------------------
-      STA velocityY ; store velocity
+      STA velocityY
+      INY
+      LDA fall_speeds,Y
+      ADC velocityY+1
+      STA velocityY+1
       RTS
+    fall_speeds:
+      .BYTE <Jump::BASE_FALL_DECCEL, >Jump::BASE_FALL_DECCEL
+      .BYTE <Jump::SLOW_FALL_DECCEL, >Jump::SLOW_FALL_DECCEL
     .ENDPROC
 
-    .PROC apply_velocity_y ; 6502 handles negative values automatically apparently?
-      LDA velocityY
-      BMI @negative
+    .PROC apply_velocity_y ; 6502 handles negative values automatically
       CLC
-      ADC positionY
+      LDA velocityY     ; low byte
+      ADC positionY     ; add signed velocity low
       STA positionY
-      LDA #0
-      ADC positionY + 1 ; add the carry to the high byte of positionY
-      STA positionY + 1
+      LDA velocityY+1   ; high byte
+      ADC positionY+1   ; add carry
+      STA positionY+1
       RTS
-    @negative:
-      LDA #0
-      SEC
-      SBC velocityY
-      STA $00
-      LDA positionY
-      SEC
-      SBC $00
-      STA positionY
-      LDA positionY + 1
-      SBC #0
-      STA positionY + 1
-      RTS
-
     .ENDPROC
 
     .PROC bound_position_y
-      ; convert from 12.4 fixed point into screen coords
-      LDA positionY
-      STA $00
-      LDA positionY + 1
-      STA $01 ; shift over four bytes
-      LSR $01
-      ROR $00
-      LSR $01
-      ROR $00      
-      LSR $01
-      ROR $00
-      LSR $01
-      ROR $00
-      LDA $00
+      LDA positionY+1
       STA spriteY
 
     @check_landing: ; This will need to be changed for collision
@@ -221,19 +216,9 @@
     @land:
         ; clamp position to the landing height
       LDA #00
-      STA positionY + 1 ; clear high byte
-      LDA #Jump::FLOOR_HEIGHT ; this will eventually be swapped with the
-                                ; landing place of collided surface
-      ASL A
-      ROL positionY + 1
-      ASL A
-      ROL positionY + 1
-      ASL A
-      ROL positionY + 1
-      ASL A
-      ROL positionY + 1 
-      STA positionY
-
+      STA positionY ; clear low byte
+      LDA #Jump::FLOOR_HEIGHT
+      STA positionY+1
       LDA #MotionState::Still ; update motion state, idk how this will
                                 ; end up working with movement
       STA motionState
@@ -245,15 +230,20 @@
       LDA btnDown
       AND #_BUTTON_RIGHT
       BEQ @check_left
-      LDA #Velocities::RIGHT_WALK_TARGET
+      LDA #<Velocities::RIGHT_WALK_TARGET
       STA targetVelocityX
+      LDA #>Velocities::RIGHT_WALK_TARGET
+      STA targetVelocityX+1
+      
       RTS
     @check_left:
       LDA btnDown
       AND #_BUTTON_LEFT
       BEQ @no_direction
-      LDA #Velocities::LEFT_WALK_TARGET
+      LDA #<Velocities::LEFT_WALK_TARGET
       STA targetVelocityX
+      LDA #>Velocities::LEFT_WALK_TARGET
+      STA targetVelocityX+1
       RTS
     @no_direction:
       LDA #0
