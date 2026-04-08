661a5758d7b2d741a124729896190487551b3625
michael <michaelsmetsker2@gmail.com>
2026-04-08 12:53:01 -0700

    a bit of work on test entity? idk it prolly needs to be refactored

--- data/entities/testEntity.s
+++ data/entities/testEntity.s
@@ -18,64 +18,83 @@ test_entity:
   .WORD update_func-1, init_func-1, remove_func-1
   .BYTE SPRITE_COUNT
 
-  ; these functions need to be passed the memory location of their ram or index of a certain pool
+sprites:
+  .BYTE $00, $00, $02, $00
+  .BYTE $00, $08, $02, $00
+  .BYTE $08, $00, $02, $00
+  .BYTE $08, $08, $02, $00
+
+  ; this proccess should only be called from the entityHandler, The memory it inherites is in the UpdateParams scope
 .PROC update_func
 
-  ; check if the sprite is within culling bounds
+  tmpSpriteX    = UpdateParams::SAFE_SCRATCH    ; 16 bit, relative x position to the screen scroll
+  tmpSpriteY    = UpdateParams::SAFE_SCRATCH+2
+
+@bound_entity:
+    ; calculate relative screen position
   SEC
   LDY #Slot::X_POS_OFFSET
   LDA (UpdateParams::slotPtr), Y
   SBC screenPosX
-  TAX ; sprite origin pixel position X relative to screen
+  STA tmpSpriteX ; low byte (pixel)
+  TAX
 
   INY
   LDA (UpdateParams::slotPtr), Y
   SBC screenPosX+1
-  BEQ @draw ; drawable if on the same screen as the player
+  STA tmpSpriteX+1 ; high byte (nametable)
+  BEQ @draw        ; drawable if on the same screen as the player
 
-  ; if not on the same screen it must fall within the threshhold not to be removed 
+    ; if not on the same screen it must fall within the threshhold not to be removed 
   TXA
-  CMP #$F1   ; Spawn point when scrolling left
+  CMP #ENTITY_SPAWN_LEFT+1   ; Spawn point when scrolling left
   BCS @draw  ; still sent to draw as following columns may be on screen
 
-  ; see if the position is on a potential spawnPoint
-  TXA ; reset cpu status
-  BEQ @check_fresh ; right of screen spawn position
-  CMP #$F0              ; left of screen spawn position 
-  BEQ @check_fresh
-  ; check fresh flag to see if its been drawn before
-
-  JMP @remove
+    ; check if position is on a spawnPoint
+  TXA                        ; reset cpu flags
+  BEQ @check_fresh           ; entity lies on right spawnpoint
+  CMP #ENTITY_SPAWN_LEFT 
+  BEQ @check_fresh           ; entity lies on left spawnpoint
+  JMP @remove                ; entity is to far offscreen, remove it
 
 @check_fresh:
+    ; see if the entity has just spawned in
   LDY #Slot::PARAM_2_OFFSET
   LDA (UpdateParams::slotPtr), Y
   AND #%10000000    ; mask the drawnFlag
-  BEQ @done         ; entity has just been spawned
+  BNE @remove       ; entity is not new
+  RTS               ; entity has just spawned, don't remove it
 
-@remove:
-  ; subtract the sprite ammount from the count
+@remove: ; TODO relocate this labels code
+    ; subtract the sprite ammount from the count
   SEC
   LDA spriteCount
   SBC #SPRITE_COUNT
   STA spriteCount
-  ; set the entity slot to inactive
+    ; set the entity slot to inactive
   LDA #$00
   TAY
   STA (UpdateParams::slotPtr), Y
   RTS
 
 @draw:
-  ; set the fresh flag
+    ; set the fresh flag
   LDY #Slot::PARAM_2_OFFSET
   LDA #%10000000
   ORA (UpdateParams::slotPtr), Y
   STA (UpdateParams::slotPtr), Y
 
-  ; draw the test sprite
+@fill_oam:
+
+  LDA tmpSpriteX+1
+  BNE @done ; FIXME this should be changed to "next row" not rts
+
+    ; draw the test sprite
   LDY #Slot::Y_POS_OFFSET
   LDA (UpdateParams::slotPtr), Y
+  STA tmpSpriteY
 
+  LDA tmpSpriteY
   LDY UpdateParams::oamOffset
   STA unreservedOam, Y
   INY
@@ -88,17 +107,16 @@ test_entity:
   STA unreservedOam, Y
   INY
 
-  TXA ; retreive hor pixel position from X register
+  TXA
   STA unreservedOam, Y
   INY
   STY UpdateParams::oamOffset
-  
+
   @done:
   RTS
 .ENDPROC
 
 .PROC init_func
-
   JSR populate_slot
   RTS
 .ENDPROC
